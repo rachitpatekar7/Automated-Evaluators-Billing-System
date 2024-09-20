726011b4cebcac34ae6af546218875d37a93b957
Hetvi Mehta <164219817+itzhetvimehta@users.noreply.github.com>
2024-09-20 22:54:50 +0530

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,27 @@
+const express = require('express');
+const cors = require('cors'); 
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+const billingRoutes = require('./routes/billingRoutes');
+const examRoutes = require('./routes/examRoutes');
+const dotenv = require('dotenv');
 
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+//const cors = require('cors');
+app.use(cors());
+
+
+// Connect to MongoDB
+connectDB();
+
+// Define Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/billing', billingRoutes);
+app.use('/api/exam', examRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
