2069998151846122f8fbcef23cc412c615959681
Hetvi Mehta <164219817+itzhetvimehta@users.noreply.github.com>
2024-11-13 10:05:39 +0530

    Add files via upload

--- backend/routes/examRoutes.js
+++ backend/routes/examRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createExam, getExams, updateExam } = require('../controllers/examController');
+const { createExam, getExams, updateExam, getAllExams } = require('../controllers/examController');
 const authMiddleware = require('../middleware/auth');
 
 const router = express.Router();
@@ -12,5 +12,8 @@ router.put('/edit/:id', authMiddleware, updateExam);
 
 // Get all exams
 router.get('/all', getExams);
+
+// Get all exams for billing, filtered by logged-in user (requires authentication)
+router.get('/all-exams', authMiddleware, getAllExams); 
 
 module.exports = router;
